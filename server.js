fdd4abef9dd7c1d97ade2235f959e5da60e47e7f
brianaabreu <briana.abreu01@gmail.com>
2020-08-13 11:50:04 -0700

    almost done

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
+require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet')
+const usersRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter')
 
 const server = express();
 
+server.use(helmet())
+server.use(logger);
+server.use(express.json());
+server.use('/api/users', usersRouter);
+server.use('/api/posts', postRouter);
+
+server.use((err, req, res, next) => {
+  console.log(err)
+
+  res.status(500).json({
+    message: "Something went wrong, please try again later",
+  })
+})
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+  res.send(`<h2>hello world</h2>`);
 });
 
-//custom middleware
+function logger(req, res, next) {
+  const method = req.method;
+  const endpoint = req.originalUrl;
+  let date = new Date();
 
-function logger(req, res, next) {}
+  console.log(`${method} to ${endpoint} at ${date}`);
+  next();
+}
+
+
 
 module.exports = server;
